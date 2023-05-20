393579dc57c251e2b95614a9416f67054de9aa59
Vasil Vangjeli <vasil_v@hotmail.com>
2023-05-20 23:19:31 +0200

    WIP - page view and rating component

--- resources/js/Pages/SingleProduct.jsx
+++ resources/js/Pages/SingleProduct.jsx
@@ -1,7 +1,7 @@
 import { Head, Link } from "@inertiajs/react";
 import MainLayout from "@/Layouts/MainLayout";
 import { useState } from "react";
-import { Breadcrumbs, Typography } from "@mui/material";
+import { Breadcrumbs, Rating, Typography } from "@mui/material";
 
 const SingleProduct = ({ book, auth, book2 }) => {
     console.log(book2);
@@ -34,46 +34,72 @@ const SingleProduct = ({ book, auth, book2 }) => {
                         </Breadcrumbs>
                     </div>
                 </div>
-                <div className="max-w-screen-2xl px-10 w-full mx-auto mt-16 flex">
-                    <div className="flex flex-row w-full border-b pb-14">
-                        <div className="w-1/5 mx-10">
-                            <img
-                                src={book[0]?.image}
-                                alt={book[0]?.image}
-                                className="w-full"
-                            />
-                        </div>
-                        <div className="w-3/5 px-10 ">
-                            <div className="flex flex-col gap-5 px-5">
-                                <div className="w-full text-4xl ">
-                                    {book[0].title}
-                                </div>
-                                <div>By author : {book[0]?.author}</div>
-                                <div className="flex flex-col gap-2 overflow-hidden pr-14">
-                                    <div>Desription</div>
-                                    <div className="">
-                                        {show
-                                            ? book[0]?.description
-                                            : book[0]?.description.substring(
-                                                  0,
-                                                  320
-                                              ) + "..."}
+                <div className="max-w-screen-2xl px-10 w-full mx-auto mt-16 flex gap-5">
+                    <div className=" w-3/4">
+                        <div className="flex flex-row border-b-2 pb-14 mr-5">
+                            <div className="w-1/3 mx-10 mxaut">
+                                <img
+                                    src={book[0]?.image}
+                                    alt={book[0]?.image}
+                                    className="w-52 mx-auto"
+                                />
+                            </div>
+                            <div className="w-2/3 px-10 ">
+                                <div className="flex flex-col gap-5 px-5">
+                                    <div className="w-full text-4xl ">
+                                        {book[0].title}
                                     </div>
-                                        <span
-                                            onClick={toggler}
-                                            className="cursor-pointer text-teal-400 text-sm font-semibold"
-                                        >
-                                            {show ? "[-] Sow less" : "[+] Show more"}
-                                        </span>
+                                    <div>By author : {book[0]?.author}</div>
+                                    <div className="flex flex-col gap-2 overflow-hidden pr-14">
+                                        <div>Desription</div>
+                                        <div className="">
+                                            {show
+                                                ? book[0]?.description
+                                                : book[0]?.description.substring(
+                                                      0,
+                                                      160
+                                                  ) + "..."}
+                                        </div>
+                                            <span
+                                                onClick={toggler}
+                                                className="cursor-pointer text-teal-400 text-sm font-semibold"
+                                            >
+                                                {show ? "[-] Sow less" : "[+] Show more"}
+                                            </span>
+                                        <div className="flex">
+                                            <Rating readOnly={false} value={book.rating} precision={1}/> 
+                                            <div>3.8</div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="w-1/5 border flex flex-col p-8 gap-5">
+                    <div className="w-1/4 border-2 flex flex-col p-10 gap-5">
                             <div className="text-lg font-semibold">Related books</div>
                             <div>{book2?.map(book =>{
-                                return <div key={book.id} className="flex flex-row gap-2">
-                                    <img src={book.image} alt={book.image} className="w-16"/>
+                                return <div key={book.id} className="flex flex-row gap-5">
+                                    <img src={book.image} alt={book.image} className="w-20 h-full"/>
+                                    <div className="flex flex-col pt-2 ">
+                                        <div className="font-semibold">{book.title}</div>
+                                        <div className="text-sm opacity-60">by {book.author}</div>
+                                        <div className="text-sm pt-2">Rating 3.8</div>
+                                    </div>
+                                </div>
+                            })}</div>
+                            <div>{book2?.map(book =>{
+                                return <div key={book.id} className="flex flex-row gap-5">
+                                    <img src={book.image} alt={book.image} className="w-20 h-full"/>
+                                    <div className="flex flex-col pt-2 ">
+                                        <div className="font-semibold">{book.title}</div>
+                                        <div className="text-sm opacity-60">by {book.author}</div>
+                                        <div className="text-sm pt-2">Rating 3.8</div>
+                                    </div>
+                                </div>
+                            })}</div>
+                            <div>{book2?.map(book =>{
+                                return <div key={book.id} className="flex flex-row gap-5">
+                                    <img src={book.image} alt={book.image} className="w-20 h-full"/>
                                     <div className="flex flex-col pt-2 ">
                                         <div className="font-semibold">{book.title}</div>
                                         <div className="text-sm opacity-60">by {book.author}</div>
